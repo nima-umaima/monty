d15cc6befccce8c049281484a6791ef5a2283b7b
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-12-15 19:18:51 +0300

    Update rotl.c

--- rotl.c
+++ rotl.c
@@ -1,1 +1,26 @@
-#inc
+#include "monty.h"
+/**
+*
+*
+*
+*
+*
+*/
+void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+{
+stack_t *tmp = *head, *aux;
+if (*head == NULL || (*head)->next == NULL)
+{
+return;
+}
+aux = (*head)->next;
+aux->prev = NULL;
+while (tmp->next != NULL)
+{
+tmp = tmp->next;
+}
+tmp->next = *head;
+(*head)->next = NULL;
+(*head)->prev = tmp;
+(*head) = aux;
+}
